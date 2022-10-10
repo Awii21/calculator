1d9d75f510c53608262d53e9b401cb4ecd6d4cb1
Awais <awaissmr@gmail.com>
2022-10-10 12:29:07 +0500

    fixed calculator.js

--- calculator.js
+++ calculator.js
@@ -1,23 +1,24 @@
 const $lis = document.querySelectorAll("ul li")
 
 $lis.forEach((node) => {
-    node.addEventListener('mousedown', function (event) {
-        const value = node.innerText.trim()
-        const $result = document.querySelector(".result")
-        const resultText = $result.innerText.trim()
-        
-        if(resultText == '0' || resultText == 'undefined' || resultText == 'infinity') {
-            $result.innerText = ''
-        }
-        
-        if (value == '=') {
-            let solution = eval(resultText)
-            $result.innerText = solution
-            return true
-        }
-        if (value.toLowerCase() == 'c') {
-            $result.innerText = ''
-            return true
-        }
-    })
+    node.addEventListener('mousedown', (event) => {
+            const value = node.innerText.trim()
+            const $result = document.querySelector(".result")
+            const resultText = $result.innerText.trim()
+
+            if (resultText == '0' || resultText == 'undefined' || resultText == 'infinity') {
+                $result.innerText = ''
+            }
+
+            if (value == '=') {
+                let solution = eval(resultText)
+                $result.innerText = solution
+                return true
+            }
+            if (value.toLowerCase() == 'c') {
+                $result.innerText = ''
+                return true
+            }
+            $result.append(value)
+        })
 })
